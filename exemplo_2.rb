b64fe7b1bf42234fe44b660b9f968f7358f1e036
jcarpim <jecarpim@gmail.com>
2020-02-24 22:23:41 -0300

    operadores

--- exemplo_2.rb
+++ exemplo_2.rb
@@ -1,4 +1,52 @@
 #operadores em ruby 
 
+# comparação:
+#  >   
+#  >=  
+#  <   
+#  <=  
+#  ==  
+#  !=  
+
+# lógicos:
+# !   
+# &&  
+# ||  
+# not 
+# and 
+# or  
+
+# matemáticos:
+# +
+# -
+# * 
+# /
+# %
+# **
+
+#controle de fluxo:
+# if
+# else
+# do
+# while
+
+#atribuindo variavel
+a,b = 1,2
+
+puts a
+puts b
+
+#testando variaveis
+if a > b
+	puts "a eh maior que b"
+else
+	puts "b eh maior que a"
+end
+
+#validação do final da linha
+puts "a eh menor que b" if a <b 
+
+
+
 
 

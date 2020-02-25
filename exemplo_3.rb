92331155ca4adfe5e40875aaee26614cfe771ac6
jcarpim <jecarpim@gmail.com>
2020-02-24 22:34:19 -0300

    input e output

---
+++ exemplo_3.rb
@@ -0,0 +1,10 @@
+#input e output
+
+#saida de dados para exibição da tela: comando puts
+puts "digite seu nome:"
+
+#entrada de dados: comando gets
+nome = gets
+
+#imprime o nome que foi imputado
+puts "O nome que voce digitou foi: "  + nome
